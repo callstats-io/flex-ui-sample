d0bbdf61fc19862483b90a21dd04acfad0362a0d
LongHDinh <long@callstats.io>
2019-07-04 11:51:44 +0300

    import appID, appSecret from app config and get UserName from twilio flex ui

--- public/assets/appConfig.sample.js
+++ public/assets/appConfig.sample.js
@@ -1,6 +1,7 @@
 // set your account sid here
-var accountSid = "AC...";
-
+const accountSid = "AC...";
+const APP_ID = 'APP_ID'
+const APP_SECRET = 'APP_SECRET'
 
 var appConfig = {
   // Uncomment the following lines to enable automatic log in
--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from "react";
 import * as Flex from "@twilio/flex-ui";
 
 class App extends React.Component {
+  componentDidMount () {
+    const APP_ID = window.$APP_ID
+    const APP_SECRET = window.$APP_SECRET
+    const userName = this.props.manager.workerClient.attributes.full_name
+
+    const script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.innerHTML = "callStats.initialize('" + APP_ID + "', '" + APP_SECRET + "', '" + userName + "');";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
   render() {
     const { manager } = this.props;
 
