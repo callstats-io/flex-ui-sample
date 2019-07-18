b03de6910b43ee86299a914af6fb78794e509390
LongHDinh <51083518+LongHDinh@users.noreply.github.com>
2019-07-18 16:23:58 +0300

    get call data from afterAcceptTask event (#1)
    * get call data from afterAcceptTask event
    
    * import appID, appSecret from app config and get UserName from twilio flex ui
    
    * docker files
    
    * docker and docker-compose file added.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import * as Flex from "@twilio/flex-ui";
 
 class App extends React.Component {
+  componentDidMount () {
+    const APP_ID = process.env.REACT_APP_ID 
+    const APP_SECRET = process.env.REACT_APP_SECRET 
+    const userName = this.props.manager.workerClient.attributes.full_name
+
+    const script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.innerHTML = "callStats.initialize('" + APP_ID + "', '" + APP_SECRET + "', '" + userName + "');";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
   render() {
     const { manager } = this.props;
 
     if (!manager) {
       return null;
     }
+    Flex.Actions.addListener("afterAcceptTask", (payload) => {
+      window.$callData = payload
+    })
 
     return (
       <Flex.ContextProvider manager={manager}>
