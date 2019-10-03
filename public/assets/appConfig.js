5578498578cb6bac82f8b831c4b76e35acb4e943
LongHDinh <long@callstats.io>
2019-10-03 09:26:25 +0300

    add appconfig file per recommendation

---
+++ public/assets/appConfig.js
@@ -0,0 +1,10 @@
+// set your account sid here
+const accountSid = "AC117d68b28074e30e608783b9083f49d5";
+
+var appConfig = {
+  // Uncomment the following lines to enable automatic log in
+  //
+  sso: {
+    accountSid: accountSid
+  }
+}
